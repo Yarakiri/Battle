ecb169354156a2c0b8a75e297757a19f00e7b7ba
Yarakiri <renou.antoine49@gmail.com>
2020-01-13 12:18:18 +0100

    contenu exo2
    exo2

--- test.c
+++ test.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
 
-
 int main(void){
-	int pv; //pv
-	int attaque;
-	pv = 100;
-	printf("MONSTRE   Pv :%d\n",pv);
+	int pv;
+	int attaque; //2
+	pv = 100; //init
+	printf("MONSTRE  :%d\n",pv );
 	printf("\n");
-	printf("Vous attaquez le monstre, il perd 15pv\n");
+	while (pv>0)
+	{
+		printf("Vous attaquez le monstre, il perd 15pv\n");
+		pv -= 15;
+		if (pv<=0){
+			pv = 0;
+			printf("MONSTRE  Pv : %d\n",pv );
+		}
+		else
+		{
+			printf("MONSTRE   Pv : %d\n",pv);
+			printf("\n");
+		}
+	}	
 	return 0;
 	
 }
