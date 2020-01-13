195c3beaede211e8db513f884d6212d960aa6a49
Yarakiri <renou.antoine49@gmail.com>
2020-01-13 13:01:02 +0100

    exo3
    exo3 nn finit

--- test.c
+++ test.c
@@ -1,13 +1,60 @@
 #include <stdio.h>
 
+int contre()
+{
+	int pv2;
+	pv2 = 200;
+		printf("Le monstre vous attaque, vous perdez 20pv\n");
+		pv2 -= 20;
+		if (pv2<=0)
+		{
+			pv2 = 0;
+			printf("VOUS  Pv : %d\n",pv2 );
+		}
+		else
+		{
+			printf("VOUS   Pv : %d\n",pv2 );
+			printf("\n");
+		}
+	return 0;
+}
+
+int attaque()
+{
+	int pv1;
+	pv1 = 100;
+		printf("Vous attaquez le monstre, il perd 15pv\n");
+		pv1 -= 15;
+		printf("%d\n",pv1 );
+		if (pv1<=0)
+		{
+			pv1 = 0;
+			printf("MONSTRE  Pv : %d\n",pv1 );
+		}
+		else
+		{
+			printf("MONSTRE   Pv : %d\n",pv1);
+			printf("\n");
+		}
+	
+	return 0;
+}
+
+
 
 int main(void){
-	int pv; //pv
-	int attaque;
-	pv = 100;
-	printf("MONSTRE   Pv :%d\n",pv);
+	int pv1;
+	int pv2;
+	pv1 = 100;
+	pv2 = 200;
+	printf("MONSTRE  :%d            ",pv1 );
+	printf("VOUS  :%d            ",pv2 );
 	printf("\n");
-	printf("Vous attaquez le monstre, il perd 15pv\n");
+	printf("\n");
+	while (pv1 >= 0 && pv2 >= 0){
+		attaque();
+		contre();	
+	}
 	return 0;
 	
 }
