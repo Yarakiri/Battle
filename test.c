59f90ad9d55bcc850de19c31fc29cea41b527d03
Yarakiri <renou.antoine49@gmail.com>
2020-01-13 11:44:13 +0100

    battle

---
+++ test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main(void){
+	int pv;
+	int attaque;
+	pv = 100;
+	printf("MONSTRE   Pv :%d\n",pv);
+	printf("\n");
+	printf("Vous attaquez le monstre, il perd 15pv\n");
+	return 0;
+	
+}
